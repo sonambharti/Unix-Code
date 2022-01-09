19e863c74f7f6e967816f3ded10c8dddef89a0d5
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-01-09 22:51:29 +0530

    Binary 2 Decimal and HexaDecimal

---
+++ Binary2DecimalHexa.sh
@@ -0,0 +1,11 @@
+echo "$((2#101))"; #!prints binary to decimal...
+
+printf '%d\n' "$((2#10101))"; #! prints binary to decimal...
+
+printf '%x\n' "$((2#10101))"; #! prints binary to hexa decimal...
+
+
+
+
+
+#https://www.onlinegdb.com/online_bash_shell
