53834eaa79090d82963a0805ea3734f1b6b64a82
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-01-09 22:50:45 +0530

    Basic commands of unix

---
+++ Basics.bash
@@ -0,0 +1,26 @@
+#                             Online Bash Shell.
+#                 Code, Compile, Run and Debug Bash script online.
+# Write your code in this editor and press "Run" button to execute it.
+
+
+
+#! is used to comment the lines inn shell script....
+
+echo "Hello World"; #!prints Hello World
+
+read input; #reads input from keyboard
+echo "$input" #prints variable 
+
+echo "$(cal)"; #!prints the calender of current present month....
+
+echo "$(cal 2010)"; #!prints the calender of given year....
+
+echo "$(cal 7 2012)"; #!prints the calender of given month of given year....
+
+echo "$(date)"; #! prints current date... Format: [day date month year time am/pm time zone]
+echo #prints new line...
+echo "$(cal -m 9 2016)"; #!prints the calender of given month of given year keeping monday as day1....
+
+
+
+
