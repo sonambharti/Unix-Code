2576fa19f23536103adaed9d214ee0b242c1bf8a
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2025-05-06 14:50:31 +0530

    Script to execute command with sudo wo entering password again and again

---
+++ run_sudo_command.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define your sudo password here (INSECURE!)
+PASSWORD="your_sudo_password_here"
+
+# Check if a command is provided
+if [ $# -eq 0 ]; then
+    echo "Executing command: $0 "
+    exit 1
+fi
+
+# Run the command with sudo, using -S to accept password from stdin
+echo "$PASSWORD" | sudo -S "$@"
+
+
+# Command to execute this file to run command <sudo apt update>
+# ./run_sudo_command.sh apt update
